4dfecb169db0595c5470682117aa7c836f32ec98
Husam <hussam.2469@gmail.com>
2023-09-26 21:46:56 +0300

    solve conflict

---
+++ Sorter.java
@@ -0,0 +1,42 @@
+public class Sorter {
+    public static void main(String[] args) {
+        int arr[] = { 64, 25, 12, 22, 11 };
+        insertionSort(arr);
+        printArray(arr);
+        selectionSort(arr);
+        printArray(arr);
+    }
+
+    public static void insertionSort(int array[]) {
+        int n = array.length;
+        for (int j = 1; j < n; j++) {
+            int key = array[j];
+            int i = j - 1;
+            while ((i > -1) && (array[i] > key)) {
+                array[i + 1] = array[i];
+                i--;
+            }
+            array[i + 1] = key;
+        }
+    }
+
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx])
+                    min_idx = j;
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+    public static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+}

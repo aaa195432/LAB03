d31923530aa9e031dcc04564bc182d229efa215e
Husam <hussam.2469@gmail.com>
2023-09-26 21:56:01 +0300

    sort method comA

--- Sorter.java
+++ Sorter.java
@@ -18,11 +18,15 @@ public class Sorter {
             arr[i] = temp;
         }
     }
-    
+
     public static void printArray(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; ++i)
             System.out.print(arr[i] + " ");
         System.out.println();
+    }
+
+    public static void sort(int array[]) {
+        selectionSort(array);
     }
 }
